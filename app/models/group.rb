646384f5f3e01f0f161bbf58c30af8d0f3f38438
Kaylei Burke <kaylei.burke@gmail.com>
2020-09-05 12:22:38 -0500

    Add organizers column to table on Groups page

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :engagements
   has_many :users, through: :engagements
+  has_many :organizers, -> { where("engagements.role": 0) }, through: :engagements,  source: :user
 
   def self.search(query)
     query = query.downcase
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :engagements
   has_many :groups, through: :engagements
+
+  def full_name
+    [self.first_name, self.last_name].join(" ")
+  end
 end
