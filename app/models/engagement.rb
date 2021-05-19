24ad09d4c46203dbd2aed405d5d2133fe7a1ca5f
Kaylei Burke <kaylei.burke@gmail.com>
2021-05-18 19:58:26 -0500

    Add sample custom validation
    Simplify has_many relationship for organizers on Group model

--- app/models/engagement.rb
+++ app/models/engagement.rb
@@ -2,7 +2,17 @@ class Engagement < ApplicationRecord
   belongs_to :user
   belongs_to :group
   enum role: [:organizer, :participant, :presenter]
-  validates :user, uniqueness: { scope: [:group, :role], message: "User/Group/Role combination already exists." }
+  validates :user, uniqueness: { scope: [:group, :role], message: "/Group/Role combination already exists." }
+  validate :no_more_than_three_organizers
+
+  def no_more_than_three_organizers
+    if self.group.organizers.size >= 3
+      errors.add(:group, "already has 3 organizers")
+    else
+      true
+    end
+
+  end
 
   def stringify
     "User: " + self.user.full_name + ", Group: " + self.group.name + ", Role: " + self.role
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,16 +1,11 @@
 class Group < ApplicationRecord
   has_many :engagements
   has_many :users, through: :engagements
-  has_many :organizers, -> { where("engagements.role": 0) }, through: :engagements,  source: :user
+  has_many :organizers, -> { where("engagements.role": :organizer) }, through: :engagements,  source: :user
   validates :name, :presence => true
 
   def self.search(query)
     query = query.downcase
     where(["LOWER(name) LIKE ?", "%#{query}%"])
-  end
-
-  def self.import(file)
-#    CSV.parse(File.read(file), headers: true)
-    #raise "Unknown file type: #{file.original_filename}"
   end
 end
