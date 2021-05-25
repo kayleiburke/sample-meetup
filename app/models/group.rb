06562c4661e542e7364113fbfc0f55ab7f43e223
Kaylei Burke <kaylei.burke@gmail.com>
2021-05-25 12:13:00 -0500

    Add uniqueness constraint to Group name

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,7 @@ class Group < ApplicationRecord
   has_many :users, through: :engagements
   has_many :organizers, -> { where("engagements.role": :organizer) }, through: :engagements,  source: :user
   validates :name, :presence => true
+  validates_uniqueness_of :name
 
   def self.search(query)
     query = query.downcase
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_072808) do
+ActiveRecord::Schema.define(version: 2021_05_25_171145) do
 
   create_table "engagements", force: :cascade do |t|
     t.integer "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_072808) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
