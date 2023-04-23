0d0a2a3d1600308957f14a121977b3705057eaae
Kaylei Burke <kaylei.burke@techstreet.com>
2023-04-23 18:15:14 -0500

    Add a Users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,37 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(sort_column(User) + " " + sort_direction).paginate(:page => params[:page], :per_page => 10)
+    @columns = [
+      {
+        field: "first_name",
+        editable: true,
+        sort_column: "first_name"
+      },
+      {
+        field: "last_name",
+        editable: true,
+        sort_column: "last_name"
+      },
+      {
+        field: "email",
+        editable: true,
+        sort_column: "email"
+      },
+      {
+        field: "actions"
+      }
+    ]
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    redirect_to edit_user_path(params[:id])
   end
 
   # GET /users/new
@@ -40,9 +62,14 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    if !params[:user][:password].blank?
+      params[:user][:password_confirmation] = params[:user][:password]
+    else
+      params[:user].delete(:password)
+    end
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { render :edit, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -69,6 +96,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.fetch(:user, {}).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :engagements
+  has_many :engagements, :dependent => :destroy
   has_many :groups, through: :engagements
   validates :first_name, presence: true
   validates :last_name, presence: true
